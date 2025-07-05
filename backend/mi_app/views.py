5baed1338303b4b0e5b1b04e8feec0d7a73433cf
Nicolas <nm5571762@gmail.com>
2025-07-05 16:32:04 -0500

    Base de datos Supabase

--- backend/mi_app/views.py
+++ backend/mi_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Usuario
+from .serializers import UsuarioSerializer
 
-# Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
